3908f3160e97c0c617772481c9e65b2968a81573
Ian_H411 <ian.h411@gmail.com>
2020-06-07 14:43:43 -0500

    initial commit

---
+++ FoodSwipe/FoodSwipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodSwipe
+//
+//  Created by Ian Hall on 6/7/20.
+//  Copyright © 2020 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
