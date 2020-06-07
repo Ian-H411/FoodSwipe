52af613b0bd99c4bafb4ec78fe00fbcd78183041
Ian_H411 <ian.h411@gmail.com>
2020-06-07 15:01:08 -0500

    adds user file and also file organizaion

---
+++ FoodSwipe/FoodSwipe/Model/userModel.swift
@@ -0,0 +1,16 @@
+//
+//  userModel.swift
+//  FoodSwipe
+//
+//  Created by Ian Hall on 6/7/20.
+//  Copyright © 2020 Ian Hall. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    init () {
+        
+    }
+    
+}
